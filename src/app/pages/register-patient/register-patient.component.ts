5125def49a45f0055ad5cb31805b26fad0d56324
Papiya Roy <papiyaro026@gmail.com>
2026-05-21 00:51:02 +0530

    Final touch

--- src/app/pages/register-patient/register-patient.component.ts
+++ src/app/pages/register-patient/register-patient.component.ts
@@ -23,8 +23,8 @@ export class RegisterPatientComponent {
     gender: new FormControl('',[Validators.required]),
     age: new FormControl<null | number>(null,[
       Validators.required,
-    Validators.minLength(0),
-  Validators.maxLength(120)]),
+    Validators.min(0),
+  Validators.max(120)]),
 
   address: new FormControl('',[
     Validators.required,
